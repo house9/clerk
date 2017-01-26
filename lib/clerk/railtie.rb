9acb8a5f4df3386d5e0a8e8734aa8e3030c82b82
Andrew Kalek <andrew.kalek@anlek.com>
2017-01-26 12:06:00 +0800

    attaching rails.logger to Clerk.configuration.logger
    This will ensure the rails logger is used by default when included into a rails app via engine.

--- lib/clerk/railtie.rb
+++ lib/clerk/railtie.rb
@@ -5,6 +5,11 @@ module Clerk
   class Railtie < Rails::Railtie
     initializer 'clerk.ar_extensions' do |app|
       ActiveRecord::Base.extend Clerk
+
+      #initialize configuration object
+      Clerk.configure do |config|
+        config.logger = Rails.logger
+      end
     end
   end
 end
